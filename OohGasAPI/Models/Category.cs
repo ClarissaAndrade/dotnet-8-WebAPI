b6fbc0b57fc45f66d881a20f499400c961c3a21a
ClarissaAndrade <clarissapa@gmail.com>
2024-01-31 22:38:52 -0300

    Commit inicial

---
+++ OohGasAPI/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace OohGasAPI.Models;
+
+public class Category
+{
+    public Category()
+    {
+        Products = new Collection<Product>();
+    }
+
+    [Key]
+    public int CategoryId { get; set; }
+    
+    [Required]
+    [StringLength(100)]
+    public string? Name { get; set; }
+
+    public ICollection<Product>? Products { get; set; }
+}
---
+++ OohGasAPI/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OohGasAPI.Models;
+
+public class Product
+{
+    [Key]
+    public int ProductId { get; set; }
+
+    [Required]
+    [StringLength(80)]
+    public string? Name { get; set; }
+
+    [Required]
+    [Column(TypeName = "decimal(10,2)")]
+    public decimal Price { get; set; }
+
+    [Required]
+    [Column(TypeName = "decimal(10,2)")]
+    public decimal Cost { get; set; }
+
+    [Column(TypeName = "decimal(10,2)")]
+    public decimal CaskPrice { get; set; }
+
+    [Column(TypeName = "decimal(10,2)")]
+    public decimal CaskCost { get; set; }
+
+    public int CategoryId { get; set; }
+    public Category? Category { get; set; }
+}
