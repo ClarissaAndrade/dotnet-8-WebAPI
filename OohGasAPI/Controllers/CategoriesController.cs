b1c7eb3b41d96e1b8d7b92136802fc28a3e6f736
ClarissaAndrade <clarissapa@gmail.com>
2025-02-11 23:21:01 -0300

    Incluindo controler Categoria

---
+++ OohGasAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OohGasAPI.Context;
+using OohGasAPI.Migrations;
+using OohGasAPI.Models;
+
+namespace OohGasAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+
+        public CategoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Category>> GetCategories()
+        {
+            return _context.Categories?.ToList() ?? [];
+        }
+
+        // GET: api/Categories/5
+        [HttpGet("{id}")]
+        public ActionResult<Category> GetCategory(int id)
+        {
+            var category = _context.Categories?.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return category;
+        }
+
+        // PUT: api/Deliverers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public IActionResult PutCategory(int id, Category category)
+        {
+            if (id != category.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(category).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Deliverers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public ActionResult<Category> PostCategory(Category category)
+        {
+            if (category == null)
+            {
+                return BadRequest(new { Message = "Os dados da categoria são inválidos." });
+            }
+
+            if (_context.Categories == null)
+            {
+                return StatusCode(500, new { Message = "Erro interno: O banco de dados não está disponível." });
+            }
+
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+
+            return CreatedAtAction("GetCategory", new { id = category.Id }, category);
+        }
+
+        // DELETE: api/Categories/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCategory(int id)
+        {
+            var category = _context.Categories?.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories?.Remove(category);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return _context.Categories?.Any(e => e.Id == id) ?? false;
+        }
+    }
+}
